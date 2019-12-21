c8e05047b63c49b04fea64d676c819683ace2ea7
wayanjimmy <jimmyeatcrab@gmail.com>
2019-12-21 07:50:05 +0700

    [gatsby] Update algolia config

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const config = {
     tweetText: "",
     links: [{ text: "", link: "" }],
     search: {
-      enabled: false,
-      indexName: "",
+      enabled: !!process.env.GATSBY_ALGOLIA_APP_ID,
+      indexName: "notebook",
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY
