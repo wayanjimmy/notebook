d8cac9e6927d995729eaa504af591cae1dee14ed
wayanjimmy <jimmyeatcrab@gmail.com>
2020-03-21 18:19:50 +0700

    config

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const config = {
     ],
     search: {
       enabled: true,
-      indexName: "",
+      indexName: "notebook",
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY
