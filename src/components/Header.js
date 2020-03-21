f461a14aa48c8a10a8d7ca128bd964cc2644b5d7
wayanjimmy <jimmyeatcrab@gmail.com>
2020-03-21 18:40:00 +0700

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { StaticQuery, graphql } from 'gatsby';
-import GitHubButton from 'react-github-btn';
-import Link from './link';
-import Loadable from 'react-loadable';
+import React from "react";
+import styled from "@emotion/styled";
+import { StaticQuery, graphql } from "gatsby";
+import GitHubButton from "react-github-btn";
+import Link from "./link";
+import Loadable from "react-loadable";
 
-import config from '../../config.js';
-import LoadingProvider from './mdxComponents/loading';
-import { DarkModeSwitch } from './DarkModeSwitch';
+import config from "../../config.js";
+import LoadingProvider from "./mdxComponents/loading";
+import { DarkModeSwitch } from "./DarkModeSwitch";
 
-const help = require('./images/help.svg');
+const help = require("./images/help.svg");
 
-const isSearchEnabled = config.header.search && config.header.search.enabled ? true : false;
+const isSearchEnabled =
+  config.header.search && config.header.search.enabled ? true : false;
 
 let searchIndices = [];
 
@@ -19,34 +20,34 @@ if (isSearchEnabled && config.header.search.indexName) {
   searchIndices.push({
     name: `${config.header.search.indexName}`,
     title: `Results`,
-    hitComp: `PageHit`,
+    hitComp: `PageHit`
   });
 }
 
-import Sidebar from './sidebar';
+import Sidebar from "./sidebar";
 
 const LoadableComponent = Loadable({
-  loader: () => import('./search/index'),
-  loading: LoadingProvider,
+  loader: () => import("./search/index"),
+  loading: LoadingProvider
 });
 
 function myFunction() {
-  var x = document.getElementById('navbar');
+  var x = document.getElementById("navbar");
 
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    x.className = 'topnav';
+    x.className = "topnav";
   }
 }
 
-const StyledBgDiv = styled('div')`
+const StyledBgDiv = styled("div")`
   height: 60px;
   box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
   background-color: #f8f8f8;
   position: relative;
   display: none;
-  background: ${props => (props.isDarkThemeActive ? '#001932' : undefined)};
+  background: ${props => (props.isDarkThemeActive ? "#001932" : undefined)};
 
   @media (max-width: 767px) {
     display: block;
@@ -76,35 +77,29 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
       }
     `}
     render={data => {
-      const logoImg = require('./images/logo.svg');
-
-      const twitter = require('./images/twitter.svg');
-
-      const discordBrandsBlock = require('./images/discord-brands-block.svg');
-
-      const twitterBrandsBlock = require('./images/twitter-brands-block.svg');
+      const twitter = require("./images/twitter.svg");
 
       const {
         site: {
-          siteMetadata: { headerTitle, githubUrl, helpUrl, tweetText, logo, headerLinks },
-        },
+          siteMetadata: {
+            headerTitle,
+            githubUrl,
+            helpUrl,
+            tweetText,
+            logo,
+            headerLinks
+          }
+        }
       } = data;
 
-      const finalLogoLink = logo.link !== '' ? logo.link : 'https://hasura.io/';
+      const finalLogoLink = logo.link !== "" ? logo.link : "https://hasura.io/";
 
       return (
-        <div className={'navBarWrapper'}>
-          <nav className={'navBarDefault'}>
-            <div className={'navBarHeader'}>
-              <Link to={finalLogoLink} className={'navBarBrand'}>
-                <img
-                  className={'img-responsive displayInline'}
-                  src={logo.image !== '' ? logo.image : logoImg}
-                  alt={'logo'}
-                />
-              </Link>
+        <div className={"navBarWrapper"}>
+          <nav className={"navBarDefault"}>
+            <div className={"navBarHeader"}>
               <div
-                className={'headerTitle displayInline'}
+                className={"headerTitle displayInline"}
                 dangerouslySetInnerHTML={{ __html: headerTitle }}
               />
             </div>
@@ -115,18 +110,18 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
               ></ul>
             ) : null}
             {isSearchEnabled ? (
-              <div className={'searchWrapper hiddenMobile navBarUL'}>
+              <div className={"searchWrapper hiddenMobile navBarUL"}>
                 <LoadableComponent collapse={true} indices={searchIndices} />
               </div>
             ) : null}
-            <div id="navbar" className={'topnav'}>
-              <div className={'visibleMobile'}>
+            <div id="navbar" className={"topnav"}>
+              <div className={"visibleMobile"}>
                 <Sidebar location={location} />
                 <hr />
               </div>
-              <ul className={'navBarUL navBarNav navBarULRight'}>
+              <ul className={"navBarUL navBarNav navBarULRight"}>
                 {headerLinks.map((link, key) => {
-                  if (link.link !== '' && link.text !== '') {
+                  if (link.link !== "" && link.text !== "") {
                     return (
                       <li key={key}>
                         <a
@@ -140,38 +135,44 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
                     );
                   }
                 })}
-                {helpUrl !== '' ? (
+                {helpUrl !== "" ? (
                   <li>
                     <a href={helpUrl}>
-                      <img src={help} alt={'Help icon'} />
+                      <img src={help} alt={"Help icon"} />
                     </a>
                   </li>
                 ) : null}
 
-                {tweetText !== '' ? (
+                {tweetText !== "" ? (
                   <li>
                     <a
-                      href={'https://twitter.com/intent/tweet?&text=' + tweetText}
+                      href={
+                        "https://twitter.com/intent/tweet?&text=" + tweetText
+                      }
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <img className={'shareIcon'} src={twitter} alt={'Twitter'} />
+                      <img
+                        className={"shareIcon"}
+                        src={twitter}
+                        alt={"Twitter"}
+                      />
                     </a>
                   </li>
                 ) : null}
-                {tweetText !== '' || githubUrl !== '' ? (
+                {tweetText !== "" || githubUrl !== "" ? (
                   <li className="divider hiddenMobile"></li>
                 ) : null}
                 {config.header.social ? (
-                  <li className={'hiddenMobile'}>
+                  <li className={"hiddenMobile"}>
                     <ul
                       className="socialWrapper"
                       dangerouslySetInnerHTML={{ __html: config.header.social }}
                     ></ul>
                   </li>
                 ) : null}
-                {githubUrl !== '' ? (
-                  <li className={'githubBtn'}>
+                {githubUrl !== "" ? (
+                  <li className={"githubBtn"}>
                     <GitHubButton
                       href={githubUrl}
                       data-show-count="true"
@@ -191,21 +192,21 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
             </div>
           </nav>
           <StyledBgDiv isDarkThemeActive={isDarkThemeActive}>
-            <div className={'navBarDefault removePadd'}>
+            <div className={"navBarDefault removePadd"}>
               <span
                 onClick={myFunction}
-                className={'navBarToggle'}
+                className={"navBarToggle"}
                 onKeyDown={myFunction}
                 role="button"
                 tabIndex={0}
               >
-                <span className={'iconBar'}></span>
-                <span className={'iconBar'}></span>
-                <span className={'iconBar'}></span>
+                <span className={"iconBar"}></span>
+                <span className={"iconBar"}></span>
+                <span className={"iconBar"}></span>
               </span>
             </div>
             {isSearchEnabled ? (
-              <div className={'searchWrapper'}>
+              <div className={"searchWrapper"}>
                 <LoadableComponent collapse={true} indices={searchIndices} />
               </div>
             ) : null}
