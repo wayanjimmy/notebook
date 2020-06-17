f9e91c55195df52355ecf9d19a7288f261b0fa72
wayanjimmy <jimmyeatcrab@gmail.com>
2020-06-17 20:49:07 +0700

    UtterancesComments

--- src/components/UtterancesComments.js
+++ src/components/UtterancesComments.js
@@ -1,23 +1,38 @@
 import React from "react";
 
-const UtterancesComments = () => (
-  <section
-    ref={elem => {
-      if (!elem) {
-        return;
-      }
+export default class UtterancesComments extends React.Component {
+  constructor(props) {
+    super(props);
 
-      const scriptElem = document.createElement("script");
-      scriptElem.src = "https://utteranc.es/client.js";
-      scriptElem.async = true;
-      scriptElem.crossOrigin = "anonymous";
-      scriptElem.setAttribute("repo", "wayanjimmy/notebook");
-      scriptElem.setAttribute("issue-term", "pathname");
-      scriptElem.setAttribute("label", "blog-comment");
-      scriptElem.setAttribute("theme", "github-light");
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+    this.component = React.createRef();
+  }
 
-export default UtterancesComments;
+  componentDidMount() {
+  const scriptElem = document.createElement("script");
+
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    '<link rel="prefetch" href="https://utteranc.es/client.js" />'
+  );
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    '<link rel="preload" href="https://utteranc.es/client.js" as="script" />'
+  );
+
+  scriptElem.src = "https://utteranc.es/client.js";
+  scriptElem.async = true;
+  scriptElem.crossOrigin = "anonymous";
+  scriptElem.setAttribute("repo", "wayanjimmy/notebook");
+  scriptElem.setAttribute("issue-term", "pathname");
+  scriptElem.setAttribute("label", "blog-comment");
+  scriptElem.setAttribute("theme", "github-light");
+
+  this.component.current.appendChild(scriptElem);
+  }
+
+  render() {
+    return (
+      <section className={this.props.className} ref={this.component} />
+    );
+  }
+}
